58919cfba9a47227a22d09c8a86a083522420df8
Sunred <sunr3d.coding@gmail.com>
2025-08-16 19:00:07 +0300

    feat(entrypoint): сборка слоев и запуск сервера

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/lib/pq v1.10.9
+	github.com/stretchr/testify v1.10.0
 	go.uber.org/zap v1.27.0
 )
 
@@ -26,8 +27,12 @@ require (
 	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/pierrec/lz4/v4 v4.1.22 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	github.com/rogpeppe/go-internal v1.14.1 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/net v0.40.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/entrypoint/entrypoint.go
+++ internal/entrypoint/entrypoint.go
@@ -1,1 +1,65 @@
 package entrypoint
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"go.uber.org/zap"
+
+	"github.com/sunr3d/order-stream-processor/internal/api"
+	"github.com/sunr3d/order-stream-processor/internal/config"
+	"github.com/sunr3d/order-stream-processor/internal/infra/inmem"
+	"github.com/sunr3d/order-stream-processor/internal/infra/postgres"
+	"github.com/sunr3d/order-stream-processor/internal/interfaces/infra"
+	"github.com/sunr3d/order-stream-processor/internal/middleware"
+	"github.com/sunr3d/order-stream-processor/internal/server"
+	"github.com/sunr3d/order-stream-processor/internal/services/order_service"
+)
+
+func Run(cfg *config.Config, logger *zap.Logger) error {
+	logger.Info("запуск приложения...")
+
+	appCtx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	// Инфра слой
+	db, err := postgres.New(cfg.Postgres, logger)
+	if err != nil {
+		return fmt.Errorf("postgres.New(): %w", err)
+	}
+	defer func(db infra.Database) {
+		if closer, ok := db.(interface{ Close() error }); ok {
+			if err := closer.Close(); err != nil {
+				logger.Error("ошибка при закрытии соединения с БД", zap.Error(err))
+			} else {
+				logger.Info("соединение с БД закрыто")
+			}
+		}
+	}(db)
+
+	cache := inmem.New(logger)
+
+	// Сервисный слой
+	svc := order_service.New(db, cache, logger)
+
+	// API слой
+	controller := api.New(svc, logger)
+	mux := http.NewServeMux()
+	controller.RegisterOrderHandlers(mux)
+
+	// Middleware слой
+	handler := middleware.Recovery(logger)(
+		middleware.ReqLogger(logger)(
+			middleware.JSONValidator(logger)(mux),
+		),
+	)
+
+	// HTTP сервер
+	srv := server.New(cfg.HTTPPort, handler, cfg.HTTPTimeout, logger)
+
+	return srv.Start(appCtx)
+}
