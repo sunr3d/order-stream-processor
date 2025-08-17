45ad21e19e7fe03324e2f76c28df8fa84d4f711a
Sunred <sunr3d.coding@gmail.com>
2025-08-17 21:52:52 +0300

    chore(validators): расширение валидаторов полей заказа

--- internal/handlers/validators/order_service.go
+++ internal/handlers/validators/order_service.go
@@ -21,10 +21,25 @@ func ValidateOrder(order *models.Order) error {
 	if strings.TrimSpace(order.TrackNumber) == "" {
 		return fmt.Errorf("track_number не может быть пустым")
 	}
+	if order.DateCreated.IsZero() {
+		return fmt.Errorf("date_created не может быть пустым")
+	}
 
 	// Поля доставки
 	if strings.TrimSpace(order.Delivery.Name) == "" {
 		return fmt.Errorf("delivery.name не может быть пустым")
+	}
+	if strings.TrimSpace(order.Delivery.Phone) == "" {
+		return fmt.Errorf("delivery.phone не может быть пустым")
+	}
+	if strings.TrimSpace(order.Delivery.Email) == "" {
+		return fmt.Errorf("delivery.email не может быть пустым")
+	}
+	if strings.TrimSpace(order.Delivery.City) == "" {
+		return fmt.Errorf("delivery.city не может быть пустым")
+	}
+	if strings.TrimSpace(order.Delivery.Address) == "" {
+		return fmt.Errorf("delivery.address не может быть пустым")
 	}
 
 	// Поля платежа
@@ -33,6 +48,15 @@ func ValidateOrder(order *models.Order) error {
 	}
 	if strings.TrimSpace(order.Payment.Provider) == "" {
 		return fmt.Errorf("payment.provider не может быть пустым")
+	}
+	if order.Payment.GoodsTotal <= 0 {
+		return fmt.Errorf("payment.goods_total не может быть меньше или равно 0")
+	}
+	if order.Payment.DeliveryCost < 0 {
+		return fmt.Errorf("payment.delivery_cost не может быть меньше 0")
+	}
+	if order.Payment.CustomFee < 0 {
+		return fmt.Errorf("payment.custom_fee не может быть меньше 0")
 	}
 	if order.Payment.Amount <= 0 {
 		return fmt.Errorf("payment.amount не может быть меньше или равно 0")
